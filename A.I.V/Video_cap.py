5f82874f9c2cea9270bae52267e35ba89b3741fa
Gustavo <gsilvagon@gmail.com.br>
2018-07-04 18:40:48 -0300

    face predictor em pasta separada

--- A.I.V/Video_cap.py
+++ A.I.V/Video_cap.py
@@ -20,7 +20,7 @@ import cv2
 # the facial landmark predictor
 print("[INFO] loading facial landmark predictor...")
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+predictor = dlib.shape_predictor("./face_predictor/shape_predictor_68_face_landmarks.dat")
 
 # initialize the video stream and allow the cammera sensor to warmup
 print("[INFO] camera sensor warming up...")
