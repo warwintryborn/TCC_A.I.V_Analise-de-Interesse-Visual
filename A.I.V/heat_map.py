963907998651d61dff83c6ff5ecf507cae29183e
Gustavo <gsilvagon@gmail.com.br>
2018-08-14 16:58:13 -0300

    Teste heatmap; Nao atualiza a imagem;

--- A.I.V/heat_map.py
+++ A.I.V/heat_map.py
@@ -32,23 +32,23 @@ class HeatMaping:
             self.__heatArray = value;
 
     def __init__(self):
-        n = 1e5
-        self.fig = PLT.figure(1)
+        PLT.figure(1)
         self.__x = self.__y = NP.linspace(0, self.__RESOLUCAO,
                                           self.__RESOLUCAO)  # Valor min, valor max, Numero de divisões
         X, Y = NP.meshgrid(self.__x, self.__y)  # Transforma em grid
         '''Quanto maior o numero de divisões mais qualidade a imagem tem'''
 
-#        Z1 = ML.bivariate_normal(X, Y, 2, 2, 0, 0)
-#        Z2 = ML.bivariate_normal(X, Y, 4, 1, 1, 1)
-
         self.__x = X.ravel()
         self.__y = Y.ravel()
-        self.__heatArray = NP.zeros(
-            self.__RESOLUCAO * self.__RESOLUCAO)  # NP.random.randn(400*400)#ZD.ravel() #Tem que ser um array com (Numero de divisões)^2
-
+        self.__heatArray = NP.zeros( self.__RESOLUCAO **2 )  # NP.random.randn(400*400)#ZD.ravel() #Tem que ser um array com (Numero de divisões)^2
+        self.go = False
         self.__gridsize = 60
+
         PLT.subplot(111)
+        PLT.hexbin(self.__x, self.__y, C=self.__heatArray, gridsize=self.__gridsize, cmap=CM.jet, bins=None)
+        PLT.axis([self.__x.min(), self.__x.max(), self.__y.min(), self.__y.max()])
+        cb = PLT.colorbar()
+        cb.set_label('mean value')
 
     def incrementa(self, x, y, raio):
         __heat = [8];
@@ -78,30 +78,27 @@ class HeatMaping:
         return;
 
     def show_map(self):
-        PLT.hexbin(self.__x, self.__y, C=self.__heatArray, gridsize=self.__gridsize, cmap=CM.jet, bins=None)
-        PLT.axis([self.__x.min(), self.__x.max(), self.__y.min(), self.__y.max()])
+        while( True ):
+            if ( self.go == False ):
+                PLT.hexbin(self.__x, self.__y, C=self.__heatArray, gridsize=self.__gridsize, cmap=CM.jet, bins=None)
+                self.go = True
 
-        cb = PLT.colorbar()
-        cb.set_label('mean value')
-        PLT.show()
-        PLT.close();
         return;
 
     def teste_heat(self):
-        self.__heatArray = NP.random.randn(self.__RESOLUCAO * self.__RESOLUCAO)#ZD.ravel() #Tem que ser um array com (Numero de divisões)^2
+        self.__heatArray = NP.random.randn(self.__RESOLUCAO ** 2)#ZD.ravel() #Tem que ser um array com (Numero de divisões)^2
+        return
 
-    def update_map(self):
-        x = NP.linspace(0, 6 * NP.pi, 100)
-        y = NP.sin(x)
-
-        ax = self.fig.add_subplot(111)
-        line1, = ax.plot(x, y, 'r-')  # Returns a tuple of line objects, thus the comma
-
-        for phase in NP.linspace(0, 10 * NP.pi, 500):
-            line1.set_ydata(NP.sin(x + phase))
-            self.fig.canvas.draw()
-            self.fig.canvas.flush_events()
         
-if (__name__ == "__main__" ):
+if (__name__ == '__main__' ):
+    from time import sleep
+    import threading as th
+
     hm = HeatMaping()
-    hm.show_map();
+    heat_thread = th.Thread(target=hm.show_map)
+    heat_thread.start();
+    while( True ):
+        sleep(10)
+        if hm.go:
+            hm.teste_heat()
+            hm.go = False
