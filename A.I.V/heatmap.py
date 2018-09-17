dcccb0ccede5aef64ba71e7f87baec505d96c598
Gustavo <gsilvagon@gmail.com.br>
2018-09-17 17:36:15 -0300

    Implementado raio do da região focada.

--- A.I.V/heatmap.py
+++ A.I.V/heatmap.py
@@ -50,58 +50,60 @@ class HeatMap:
             PLT.set_cmap(self.__HEATTYPE)
 
             ax = self.fig.add_subplot(111)
-            self.__heatValue = NP.zeros((self.__HIGHT, self.__WIDTH))#random.random((self.__HIGHT, self.__WIDTH))
+            self.__heatValue = NP.zeros((self.__HIGHT, self.__WIDTH))
             self.im = ax.imshow(self.__heatValue,alpha=0.5)
             # self.im = ax.imshow(img,alpha=0.4)
             self.clb = self.fig.colorbar(self.im, ticks=[]);
 
             self.config_heat_fig();
-            PLT.show(block=False)
-            PLT.pause(0.01)
+
+            self.show_map();
         except:
             error = sys.exc_info()[0]
             print("Unexpected error:", error)
 
     def incrementa(self, x, y):
 
-        self.heatArray[x][y] = self.__VALOR_INCREMENTAL
-        self.__VALOR_INCREMENTAL += 5
-        self.IS_CHANGED = True;
-        return;
-
-        __heat = [8];
-
         if (0 > x > self.__WIDTH or 0 > y > self.__HIGHT):
             #Adicionar mensagem de erro!!
             return;
 
-        # local = (x * self.__RESOLUCAO + y * self.__RESOLUCAO)
+        self.__heatValue[x][y] += self.__VALOR_INCREMENTAL;  # centro
 
+        for i in range(1, self.__RAIO):
 
-        for i in range(0, self.__RAIO):
+            direita = x + i;
+            esquerda = x - i;
+            cima = y + i;
+            baixo = y - i;
 
-            self.__heatValue[x + i][0] = self.__VALOR_INCREMENTAL;                  # direita
-            self.__heatValue[x - i][0] = self.__VALOR_INCREMENTAL;                  # esquerda
-            self.__heatValue[0][y + i] = self.__VALOR_INCREMENTAL;                  # cima
-            self.__heatValue[0][y - i] = self.__VALOR_INCREMENTAL;
-            self.__heatValue[x + i][y + i] = self.__VALOR_INCREMENTAL_PERIFERIA;    # direita em cima
-            self.__heatValue[x - i][y + i] = self.__VALOR_INCREMENTAL_PERIFERIA;    # esquerda em cima
-            self.__heatValue[x - i][y + i] = self.__VALOR_INCREMENTAL_PERIFERIA;    # direita em baixo
-            self.__heatValue[x - i][y - i] = self.__VALOR_INCREMENTAL_PERIFERIA;    # esquerda em baixo
+            if( direita < self.__HIGHT ):
+                self.__heatValue[direita][y] += self.__VALOR_INCREMENTAL;  # direita
+                if (cima < self.__WIDTH):
+                    self.__heatValue[direita][cima] += self.__VALOR_INCREMENTAL_PERIFERIA;  # direita em cima
+                if (0 < baixo):
+                    self.__heatValue[direita][baixo] += self.__VALOR_INCREMENTAL_PERIFERIA;  # direita em baixo
 
-        for posicao in __heat:
+            if( 0 < esquerda ):
+                self.__heatValue[esquerda][y] += self.__VALOR_INCREMENTAL;  # esquerda
+                if (cima < self.__WIDTH):
+                    self.__heatValue[esquerda][cima] += self.__VALOR_INCREMENTAL_PERIFERIA;  # esquerda em cima
+                if (0 < baixo):
+                    self.__heatValue[esquerda][baixo] += self.__VALOR_INCREMENTAL_PERIFERIA;  # esquerda em baixo
 
-            if (posicao > self.__RESOLUCAO or posicao < 0):
-                continue;
+            if( cima < self.__WIDTH ):
+                self.__heatValue[x][cima] += self.__VALOR_INCREMENTAL;  # cima
 
-            self.__heatValue[posicao] = self.__VALOR_INCREMENTAL;
+            if( 0 < baixo):
+                self.__heatValue[x][baixo] += self.__VALOR_INCREMENTAL;  # baixo
+
+        self.IS_CHANGED = True;
+        self.show_map();
 
         return;
 
     def show_map(self):
         try:
-            #Transfor para numpy array
-            # self.__heatValue = NP.random.random((self.__HIGHT, self.__WIDTH))
             self.im.set_array(self.__heatValue)
             self.fig.canvas.draw()
 
@@ -118,11 +120,6 @@ class HeatMap:
             print("Unexpected error:", error)
         return
 
-
-    def teste_heat(self):
-        self.__heatValue = NP.random.random((self.__HIGHT, self.__WIDTH))
-        return
-
     def config_heat_fig(self):
 
         titulo_janela = PLT.gcf()
@@ -130,15 +127,28 @@ class HeatMap:
         PLT.axis('off');
         PLT.suptitle("A.I.V.", fontsize=32);
         PLT.title("Vitrine", fontsize=12);
-        PLT.colorbar()
+
+    def reset_map(self):
+        self.__heatValue = NP.zeros((self.__HIGHT, self.__WIDTH))  # random.random((self.__HIGHT, self.__WIDTH))
+        self.show_map();
 
 if (__name__ == '__main__' ):
 
     hm = HeatMap()
+    hm2 = HeatMap()
 
-    for i in range(14, -1, -1):
-        for k in range(30):
-            hm.incrementa(i, k)
-            hm.show_map()
+    hm.incrementa(5, 13)
+    # hm.show_map()
 
-    hm.show_map()
+    hm2.incrementa(10, 8)
+    # hm2.show_map()
+
+    # for i in range(30):
+    #     for k in range(15):
+    #         hm.incrementa(k, i)
+    #         hm.show_map()
+
+    hm.reset_map()
+    hm2.reset_map();
+
+    exit()
